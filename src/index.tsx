d88a5e89fff9edf4a84b917c267935fd8824bc39
Nicolas Carlo <nicolascarlo.espeon@gmail.com>
2017-03-06 20:02:17 +0100

    Bootstrap project with tools

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+
+const render = () => {
+  ReactDOM.render(
+    <div>Toto, world!</div>,
+    document.getElementById('root')
+  );
+};
+
+render()
